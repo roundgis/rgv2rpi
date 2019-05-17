290a9c269bca60e664b99da2ae610bf392ea6ebc
roundgis <roundgis@roundgis.com>
2019-05-17 16:58:08 +0800

    u

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-LOG_PATH = "/home/mathgl/rglog"
+LOG_PATH = "/home/pi/rglog"
 HOST = "rg3.esis.vip"
 HTTP_PORT = 9004
 URL_PREFIX = r'/rgv'
@@ -10,8 +10,8 @@ REDIS = {
     "port": 21999
 }
 WEB = {
-    "static_path": "/home/mathgl/rgw_web",
-    "export_path": "/home/mathgl/rglog",
+    "static_path": "/home/pi/rgw_web",
+    "export_path": "/home/pi/rglog",
     "login_page_bkg": "login_roundgis_bkg3.png",
     'js_dir': "rgv_js",
     'template_dir': "rgv_templates",
@@ -20,12 +20,12 @@ WEB = {
 }
 
 BIZ_DB = {
-    "path": "/home/mathgl/rgv_biz.db3",
+    "path": "/home/pi/rgv_biz.db3",
     "ttl": 3*86400
 }
 
 LOG_DB = {
-    "path": "/home/mathgl/rgv_log.db3",
+    "path": "/home/pi/rgv_log.db3",
     "ttl": 32*86400
 }
 
